0e9e327a0dd4b55f26b832827b9b9a0e8d5cfc93
Poojan <poojangoyani@gmail.com>
2025-10-05 17:12:20 +0530

    removed build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", images: { unoptimized: true } };
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
